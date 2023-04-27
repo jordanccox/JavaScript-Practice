398ec917b4028534f4b021691eb30ae795bbc1c7
Jordan Cox <jordancox747@outlook.com>
2023-04-27 17:09:53 -0600

    new commit

--- Sorting Algorithms/add-binary-integers.js
+++ Sorting Algorithms/add-binary-integers.js
@@ -1,6 +1,6 @@
 // Arrays A and B store some integer in n-bits
 
-function addBinaryIntegers(A, B, n) {
+function addBinaryIntegers(A, B) {
     let a = 0;
     let b = 0;
 
@@ -10,10 +10,8 @@ function addBinaryIntegers(A, B, n) {
     B.reverse();
 
     // Convert binary integers into decimal integers
-    for (let i = 0; i < n; i++) {
-        a = a + (A[i] * Math.pow(2, i));
-        b = b + (B[i] * Math.pow(2, i));
-    }
+    a = convertBinaryToDecimal(A);
+    b = convertBinaryToDecimal(B);
 
     // Add decimal integers;
     let c = a + b;
@@ -21,20 +19,33 @@ function addBinaryIntegers(A, B, n) {
     // Convert c to binary array C;
     const C = [];
 
-    for (let i = 0; i <= n; i++) {
-        C[n - i] = c % 2;
+    while (c > 0) {
+        C.unshift(c % 2);
         c = Math.floor(c / 2);
     }
     
     return C;
 }
 
+function convertBinaryToDecimal(array) {
+    let a = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        a = a + (array[i] * Math.pow(2, i));
+    }
+
+    return a;
+}
+
 const binary8 = [1, 0, 0, 0];
 const binary9 = [1, 0, 0, 1];
+const binary44 = [1, 0, 1, 1, 0, 0];
+const binary43 = [1, 0, 1, 0, 1, 1];
+const binary87 = addBinaryIntegers(binary44, binary43);
+const binary17 = addBinaryIntegers(binary8, binary9);
+const binary34 = addBinaryIntegers(binary17, binary17).join("");
+const binary96 = addBinaryIntegers(binary9, binary87);
 
-const binary17 = addBinaryIntegers(binary8
-    , binary9, 4);
-
-const binary34 = addBinaryIntegers(binary17, binary17, 5);
-
-console.log(binary17);
+console.log(binary34);
+console.log(binary87);
+console.log(binary96);
---
+++ Sorting Algorithms/selection-sort.js
@@ -0,0 +1,26 @@
+function selectionSort(array) {
+
+    for (let i = 0; i < array.length; i++) {
+        let min = array[i];
+        let j = i + 1;
+        let index = i;
+
+        while (j < array.length) {
+            if (array[j] < min) {
+                min = array[j];
+                index = j;
+            }
+            j++;
+        }
+
+        let temp = array[i];
+        array[i] = min;
+        array[index] = temp;
+    }
+
+    return array;
+}
+
+const arr = [8, 3, 2, 1, 6, 3, 4, 9, 5, 2, 7, 11, 14, 29];
+
+console.log(selectionSort(arr));
